dc5035887e6034109385bde00989fb0c31346ad4
Shanger1 <michkun22@gmail.com>
2016-10-12 12:29:36 +0200

    Currency added

--- src/main/java/domain/Franc.java
+++ src/main/java/domain/Franc.java
@@ -1,19 +1,13 @@
 package domain;
 
-public class Franc {
+public class Franc extends Currency{
 
-	private int amount;
 	public Franc(int amount) {
 		this.amount=amount;
 	}
 
 	public Franc times(int multiplier) {
 		return new Franc(this.amount*multiplier);
-	}
-	
-	public boolean equals(Object other){
-		Franc franc = (Franc) other;
-		return franc.amount==this.amount;
 	}
 
 }
