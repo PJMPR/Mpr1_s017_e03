eaca540304ed317b17b6e86e293c606d7b15b27c
student <student@virtualbox>
2016-10-19 12:43:56 +0200

    added sum

---
+++ src/main/java/domain/Sum.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Sum implements Expression {
+
+	public Sum(Money augend, Money addend) {
+		this.augend=augend;
+		this.addend=addend;
+	}
+	
+	public Money addend;
+	public Money augend;
+
+}
