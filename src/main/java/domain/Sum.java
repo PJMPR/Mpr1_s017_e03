3e66b48295634d1bdb8d33994ec114412457c360
Shanger1 <student@virtualbox>
2016-10-19 12:42:31 +0200

    sum class

---
+++ src/main/java/domain/Sum.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Sum implements Expression {
+
+	public Money augend;
+	public Money addend;
+
+	public Sum(Money augend, Money addend) {
+		this.augend = augend;
+		this.addend = addend;
+	}
+
+}
