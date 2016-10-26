226031d442868151c70e6fce815145b955b18708
Eczu <mer.kuba@gmail.com>
2016-10-26 20:11:17 +0200

    Fixed person class

--- src/main/java/domain/model/Person.java
+++ src/main/java/domain/model/Person.java
@@ -1,12 +1,11 @@
 package domain.model;
 
+
 import java.util.*;
 
 public class Person {
 
 	private String pesel;
-	private int age;
-	private Date DayOfBirth;
 	public enum Gender{
 		Male, Female
 	}
@@ -23,19 +22,86 @@ public class Person {
 		this.pesel = pesel;
 	}
 
-	public Date getDayOfBirth(){
+	public Calendar getDayOfBirth(){
+
+
+        if(Character.getNumericValue(getPesel().charAt(2)) > 1  && Character.getNumericValue(getPesel().charAt(2)) < 5 ) {
+            StringBuilder yearOfBirth = new StringBuilder();
+            yearOfBirth.append("2" + "0" + getPesel().charAt(0) + getPesel().charAt(1));
+            String sYear = yearOfBirth.toString();
+            Integer birthYear = Integer.valueOf(sYear);
+
+            StringBuilder monthOfBirth = new StringBuilder();
+            monthOfBirth.append("" + Character.getNumericValue(getPesel().charAt(2)-2) + getPesel().charAt(3));
+            String sMonth = monthOfBirth.toString();
+            Integer birthMonth = Integer.valueOf(sMonth);
+
+            StringBuilder dayOfBirth = new StringBuilder();
+            dayOfBirth.append("" + getPesel().charAt(4)+ getPesel().charAt(5));
+            String sDay = dayOfBirth.toString();
+            Integer birthDay = Integer.valueOf(sDay);
+
+
+
+
+
+            return new GregorianCalendar(birthYear,birthMonth, birthDay);
+
+        }
+        else if (Character.getNumericValue(getPesel().charAt(2)) <= 1)
+        {
+            StringBuilder yearOfBirth = new StringBuilder();
+            yearOfBirth.append("1" + "9" + getPesel().charAt(0) + getPesel().charAt(1));
+            String sYear = yearOfBirth.toString();
+            Integer birthYear = Integer.valueOf(sYear);
+
+            StringBuilder monthOfBirth = new StringBuilder();
+            monthOfBirth.append("" + getPesel().charAt(2) +  getPesel().charAt(3));
+            String sMonth = monthOfBirth.toString();
+            Integer birthMonth = Integer.valueOf(sMonth);
+
+            StringBuilder dayOfBirth = new StringBuilder();
+            dayOfBirth.append("" + getPesel().charAt(4) + getPesel().charAt(5));
+            String sDay = dayOfBirth.toString();
+            Integer birthDay = Integer.valueOf(sDay);
+
+            return new GregorianCalendar(birthYear,birthMonth,birthDay);
+        }
 
         return null;
+
     }
 
 	public int getAge(){
-        return 20;
+        Calendar now = new GregorianCalendar();
+        int year = now.get(Calendar.YEAR);
+
+        if(Character.getNumericValue(getPesel().charAt(2)) > 1  && Character.getNumericValue(getPesel().charAt(2)) < 5 ) {
+           StringBuilder yearOfBirth = new StringBuilder();
+           yearOfBirth.append("2" + "0" + getPesel().charAt(0) + getPesel().charAt(1));
+           String s = yearOfBirth.toString();
+            Integer birthYear = Integer.valueOf(s);
+            return  year - birthYear;
+
+        }
+        else if (Character.getNumericValue(getPesel().charAt(2)) <= 1)
+        {
+            StringBuilder yearOfBirth = new StringBuilder();
+            yearOfBirth.append("1" + "9" + getPesel().charAt(0) + getPesel().charAt(1));
+            String s = yearOfBirth.toString();
+            Integer birthYear = Integer.valueOf(s);
+            return  year - birthYear;
+        }
+
+        else return 0;
 
 
 	}
 
 	public Gender getGender(){
-        if (getPesel().charAt(10)%2 == 1 ){
+
+
+        if (Character.getNumericValue(getPesel().charAt(9))%2 == 1 ){
             return Gender.Male;
         }
         else {
@@ -43,22 +109,26 @@ public class Person {
         }
 	}
 
-	public boolean checkPesel(){
+    public int validatePesel(){
         List<Integer> peselValidator = new ArrayList();
         peselValidator.add(1);
         peselValidator.add(3);
         peselValidator.add(7);
         peselValidator.add(9);
 
-        int verify  = 0;
-        for(int k=1; k<11; k++){
-            verify = getPesel().charAt(k) + getPesel().charAt(k)* peselValidator.get(k % 4) % 10;
+        int verify = 0;
+
+        for (int k = 0; k <= 9; k++) {
+            verify = (verify + Character.getNumericValue(getPesel().charAt(k)) * peselValidator.get(k % 4)) % 10;
 
         }
 
+        int check = (10-verify) % 10;
+        return check;
 
-
-        if(getPesel().length() == 11 && (10 - verify)%10 == 0  ){
+    }
+	public boolean checkPesel(){
+        if(getPesel().length() == 11 &&  Character.getNumericValue(getPesel().charAt(10)) == validatePesel()   ){
             return true;
         }
         else
