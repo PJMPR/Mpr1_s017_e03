c397d824f4ea36d8e8da4deb6cf6ecf2479d50fc
Eczu <mer.kuba@gmail.com>
2016-10-26 17:37:10 +0200

    Added tests and Person class

--- src/main/java/domain/model/Person.java
+++ src/main/java/domain/model/Person.java
@@ -1,10 +1,19 @@
 package domain.model;
 
-import java.util.Date;
+import java.util.*;
 
 public class Person {
-	
+
 	private String pesel;
+	private int age;
+	private Date DayOfBirth;
+	public enum Gender{
+		Male, Female
+	}
+
+    public Person(){
+    String pesel;
+    }
 
 	public String getPesel() {
 		return pesel;
@@ -13,16 +22,48 @@ public class Person {
 	public void setPesel(String pesel) {
 		this.pesel = pesel;
 	}
-	
-	//public Date getDayOfBirth(){}
-	
-	//public int getAge(){}
-	//public Gender getGender(){}
-	
-	//public boolean checkPesel(){}
-	
+
+	public Date getDayOfBirth(){
+
+        return null;
+    }
+
+	public int getAge(){
+        return 20;
+
+
+	}
+
+	public Gender getGender(){
+        if (getPesel().charAt(10)%2 == 1 ){
+            return Gender.Male;
+        }
+        else {
+            return Gender.Female;
+        }
+	}
+
+	public boolean checkPesel(){
+        List<Integer> peselValidator = new ArrayList();
+        peselValidator.add(1);
+        peselValidator.add(3);
+        peselValidator.add(7);
+        peselValidator.add(9);
+
+        int verify  = 0;
+        for(int k=1; k<11; k++){
+            verify = getPesel().charAt(k) + getPesel().charAt(k)* peselValidator.get(k % 4) % 10;
+
+        }
+
+
+
+        if(getPesel().length() == 11 && (10 - verify)%10 == 0  ){
+            return true;
+        }
+        else
+            return false;
+	}
+
 }
 
-enum Gender{
-	Male, Female
-}
