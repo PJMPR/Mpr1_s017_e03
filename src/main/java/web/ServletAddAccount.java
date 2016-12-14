384bf723f6ac729f9a1071aa0fc153e1286c9492
student <student@virtualbox>
2016-12-14 11:48:44 +0100

    add account session

--- src/main/java/web/ServletAddAccount.java
+++ src/main/java/web/ServletAddAccount.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.model.Account;
+import domain.model.Person;
 
 @WebServlet(urlPatterns="/addAccount")
 public class ServletAddAccount extends HttpServlet {
@@ -18,6 +22,8 @@ public class ServletAddAccount extends HttpServlet {
 			HttpServletResponse response) throws IOException{
 		
 		response.setContentType("text/html");
+		
+		HttpSession session = request.getSession();
 
 		String currency = request.getParameter("currency");
 		String amount = request.getParameter("amount");
@@ -29,6 +35,15 @@ public class ServletAddAccount extends HttpServlet {
 				+ amount
 				+ "</h1>");
 		out.close();
+		
+		Person person = (Person) session.getAttribute("person");
+		if(person != null){
+			Account account = new Account();
+			account.setAmount(Double.parseDouble(amount));
+			account.setCurrency(currency);
+			account.setPerson(person);
+			session.setAttribute("account", account);
+		}
 	}
 	
 
