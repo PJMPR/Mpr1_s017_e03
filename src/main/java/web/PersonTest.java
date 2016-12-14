7a52a9bd46a83a5f89f4c6ef11d3437cac00a439
student <student@virtualbox>
2016-12-14 12:40:01 +0100

    redirect

--- src/main/java/web/PersonTest.java
+++ src/main/java/web/PersonTest.java
@@ -1,5 +1,7 @@
 package web;
 
+import domain.model.Person;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns="/addPerson")
 public class PersonTest extends HttpServlet{
@@ -18,7 +21,20 @@ private static final long serialVersionUID = 1L;
 			HttpServletResponse response) throws IOException{
 		
 		response.setContentType("text/html");
-		
+		HttpSession session = request.getSession();
+		String nameSession = request.getParameter("name");
+		String surnameSession = request.getParameter("surname");
+
+        Person person = new Person();
+        person.setName(nameSession);
+        person.setSurname(surnameSession);
+
+
+		if (   !person.getName().equals("") && !person.getSurname().equals("") && !person.getName().equals(null) && !person.getSurname().equals(null)){
+			session.setAttribute("person", person);
+		}
+
+
 		String name = request.getParameter("name");
 		String surname = request.getParameter("surname");
 		PrintWriter out = response.getWriter();
--- src/main/java/web/ServletAddAccount.java
+++ src/main/java/web/ServletAddAccount.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.model.Account;
+import domain.model.Person;
 
 @WebServlet(urlPatterns="/addAccount")
 public class ServletAddAccount extends HttpServlet {
@@ -18,17 +22,23 @@ public class ServletAddAccount extends HttpServlet {
 			HttpServletResponse response) throws IOException{
 		
 		response.setContentType("text/html");
+		
+		HttpSession session = request.getSession();
 
 		String currency = request.getParameter("currency");
 		String amount = request.getParameter("amount");
-		PrintWriter out = response.getWriter();
-		out.println("<h1>Choosen currency: "
-				+ currency
-				+ "</br>"
-				+ "Choosen amount: "
-				+ amount
-				+ "</h1>");
-		out.close();
+		
+		Person person = (Person) session.getAttribute("person");
+		if(person != null){
+			Account account = new Account();
+			account.setAmount(Double.parseDouble(amount));
+			account.setCurrency(currency);
+			account.setPerson(person);
+			session.setAttribute("account", account);
+		}
+		else{
+			response.sendRedirect("/addPerson.html");
+		}
 	}
 	
 
