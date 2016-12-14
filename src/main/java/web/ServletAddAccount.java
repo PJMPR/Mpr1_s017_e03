ea8a55ebd61f66f098097d558db5a4c3bc7bb47c
student <student@virtualbox>
2016-12-14 11:44:18 +0100

    merge

--- src/main/java/web/ServletAddAccount.java
+++ src/main/java/web/ServletAddAccount.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns="/addAccount")
 public class ServletAddAccount extends HttpServlet {
@@ -19,20 +18,17 @@ public class ServletAddAccount extends HttpServlet {
 			HttpServletResponse response) throws IOException{
 		
 		response.setContentType("text/html");
-		
-		HttpSession session = request.getSession();
-		
+
 		String currency = request.getParameter("currency");
-		String value = request.getParameter("value");
+		String amount = request.getParameter("amount");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>Choosen currency: "
 				+ currency
-				+ "Choosen value"
-				+ value
+				+ "</br>"
+				+ "Choosen amount: "
+				+ amount
 				+ "</h1>");
 		out.close();
-		
-		
 	}
 	
 
