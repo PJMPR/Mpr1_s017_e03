34c861c4585e639326df3f97fa1ae3f1479dedcc
student <student@virtualbox>
2016-12-21 11:49:40 +0100

    filtr

---
+++ src/main/java/web/filters/DemoFilter.java
@@ -0,0 +1,40 @@
+package web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns={"test"})
+public class DemoFilter implements Filter{
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		if(req.getParameter("name")==null){
+			resp.sendRedirect("index.html");
+		}else{
+			chain.doFilter(req, resp);
+		}
+		
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
