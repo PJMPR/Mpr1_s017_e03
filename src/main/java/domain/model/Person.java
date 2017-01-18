ed976221fb783776064721e3b1b235815b714f37
student <student@virtualbox>
2017-01-18 11:20:46 +0100

    hibernate setup for Person and EnumDict

--- src/main/java/domain/model/Person.java
+++ src/main/java/domain/model/Person.java
@@ -2,12 +2,32 @@ package domain.model;
 
 import java.util.List;
 
+import javax.annotation.Generated;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "person.all", query = "Select p from Person p"),
+	@NamedQuery(name = "person.name", query = "Select p from Person p where p.name = name"),
+	@NamedQuery(name = "person.id", query = "Select p from Person p where p.id = id")
+})
 public class Person implements IHaveId{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String surname;
+	
+	@Transient
 	private List<Account> accounts;
 	
 	public String getName() {
@@ -22,6 +42,8 @@ public class Person implements IHaveId{
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+	
+	@OneToMany(cascade = CascadeType.ALL)
 	public List<Account> getAccounts() {
 		return accounts;
 	}
